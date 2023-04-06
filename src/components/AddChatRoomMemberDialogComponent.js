540e26372814be2a0a1b08150d31f27bdc0f12ff
LeonAdeoye <leon.adeoye@gmail.com>
2023-04-06 20:17:09 +0800

    REST calls

---
+++ src/components/AddChatRoomMemberDialogComponent.js
@@ -0,0 +1,101 @@
+import React, {Component} from 'react';
+import {Button, Dialog, DialogTitle, DialogContent, DialogActions, TextField} from '@mui/material'
+import {connect} from "react-redux";
+import {toggleAddChatRoomMemberDialogFlag} from "../redux/room/roomActions";
+import {Autocomplete} from "@mui/lab";
+import {fetchUsers} from "../redux/user/userActions";
+import {addMemberToRoom} from "../redux/roomList/roomListActions";
+
+class AddChatRoomMemberDialogComponent extends Component
+{
+    constructor(props)
+    {
+        super(props);
+        this.state = {
+            newRoomMemberId: ''
+        }
+    }
+
+    componentDidMount()
+    {
+        this.props.fetchUsers();
+    }
+
+    render()
+    {
+        const {openAddChatRoomMemberDialogFlag, toggleAddChatRoomMemberDialogFlag, users, addMemberToRoom, loggedInUserId, selectedRoom} = this.props;
+
+        const handleOnChangeEvent = (event, newValue) =>
+        {
+            this.setState({newRoomMemberId: newValue});
+        }
+
+        const handleCancel= () =>
+        {
+            toggleAddChatRoomMemberDialogFlag();
+        }
+
+        const handleSubmit = () =>
+        {
+            toggleAddChatRoomMemberDialogFlag();
+            addMemberToRoom(selectedRoom.id, this.state.newRoomMemberId, loggedInUserId);
+        }
+
+        return (
+            <div>
+                <Dialog aria-labelledby='dialog-title'
+                        width='500px'
+                        height='80px'
+                        sx={{ backgroundColor: '#404040'}}
+                        open={openAddChatRoomMemberDialogFlag}>
+                    <DialogTitle id='dialog-title' sx={{ backgroundColor: 'white', color: '#404040'}} >New Member In {selectedRoom.roomName}</DialogTitle>
+                    <DialogContent sx={{ width: '500px', height: '80px', backgroundColor: '#404040', color: 'lightgrey'}}>
+                        <Autocomplete size='small'
+                                      renderInput={(params) => <TextField {...params} label='Select member to add room' />}
+                                      options={users.map(user => user.fullName)}
+                                      value={this.state.newRoomMemberId}
+                                      onChange={handleOnChangeEvent}
+                                      freeSolo
+                                      variant='outlined'
+                                      width='70%'
+                                      InputLabelProps={{ style: { color: 'white' } }}
+                                      inputProps={{ style: { color: 'white'} }}
+                                      sx={{mt:2, mb:2, mr:2, ml:2, width:'90%', backgroundColor:'#575555', borderColor:'white'}}
+                        />
+                    </DialogContent>
+                    <DialogActions sx={{ backgroundColor: '#404040'}}>
+                        <Button variant='outlined' sx={{ backgroundColor: '#404040', borderColor:'white', color: 'white', '&:hover': {
+                                backgroundColor: '#4f4e4e',
+                                borderColor:'white'
+                            }}} onClick={handleCancel}>Cancel</Button>
+                        <Button variant='outlined' sx={{ backgroundColor: '#404040', borderColor:'white', color: 'white', '&:hover': {
+                                backgroundColor: '#4f4e4e',
+                                borderColor:'white'
+                            }}} autoFocus onClick={handleSubmit}>Submit</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state, ownProps) =>
+{
+    return {
+        openAddChatRoomMemberDialogFlag: state.room.openAddChatRoomMemberDialogFlag,
+        users: state.user.users,
+        loggedInUserId: state.user.loggedInUserId,
+        selectedRoom: state.roomList.selectedRoom
+    }
+}
+
+const mapDispatchToProps = (dispatch, ownProps) =>
+{
+    return {
+        toggleAddChatRoomMemberDialogFlag: () => dispatch(toggleAddChatRoomMemberDialogFlag()),
+        fetchUsers: () => dispatch(fetchUsers()),
+        addMemberToRoom: (selectedRoomId, newRoomMemberId, loggedInUserId) => dispatch(addMemberToRoom(selectedRoomId, newRoomMemberId, loggedInUserId))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (AddChatRoomMemberDialogComponent);
